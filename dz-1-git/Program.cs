3116e3b4148b37205468f58b93f5b3aceb1b66f9
pollosatyi <88507116+pollosatyi@users.noreply.github.com>
2024-11-20 21:08:30 +0300

    Add project files.

---
+++ dz-1-git/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dz_1_git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Это основная ветка");
+        }
+    }
+}
